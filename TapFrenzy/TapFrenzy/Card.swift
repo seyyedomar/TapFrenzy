8c18e436cd5e18f1d0f4a15057f80324404b9710
seyyedomar <oruzaik@gmail.com>
2026-07-08 10:39:23 +0530

    Add Card model for Light It Up grid

---
+++ TapFrenzy/TapFrenzy/Card.swift
@@ -0,0 +1,13 @@
+//
+//  Card.swift
+//  TapFrenzy
+//
+//  Created by Seyyed Omar on 2026-07-08.
+
+
+import Foundation
+
+struct Card: Identifiable {
+    let id = UUID()
+    var isLit: Bool = false
+}
